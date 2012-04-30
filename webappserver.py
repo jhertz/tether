a2eea04927d0869a1c182f5557e9731f6c215561
hcreynol <hobart_reynolds@brown.edu>
2012-04-30 19:53:04 -0400

    improved webapp and python for serving it

--- webappserver.py
+++ webappserver.py
@@ -1,5 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 
+import os
 import sys
 sys.path.append('tornado-2.2.1/')
 
@@ -8,15 +9,16 @@ import tornado.ioloop
 
 SERVER_PORT = 80
 
-#class ServerHandler(tornado.web.RequestHandler):
-#	def get(self):
-#		f = open('webapp.html', 'r')
-#		for line in f:
-#			self.write(line)
+class ServerHandler(tornado.web.RequestHandler):
+	def get(self):
+		f = open('webapp.html', 'r')
+		for line in f:
+			self.write(line)
+		f.close()
 
 def mainLoop():
 	print "CHECKPOINT 1", "Listening on port", SERVER_PORT
-	application = tornado.web.Application([ (r"/", tornado.web.StaticFileHandler), ])
+	application = tornado.web.Application([ (r"/", ServerHandler) ] )
 	application.listen(SERVER_PORT)
 	tornado.ioloop.IOLoop.instance().start()
 
