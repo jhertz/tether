7e85ded058dee6bafc4fd6f3f7d0b544efa05b33
hcreynol <hobart_reynolds@brown.edu>
2012-04-30 18:41:14 -0400

    adding webappserver python file

---
+++ webappserver.py
@@ -0,0 +1,23 @@
+# -*- coding: iso-8859-1 -*-
+
+import sys
+sys.path.append('tornado-2.2.1/')
+
+import tornado.web
+import tornado.ioloop
+
+SERVER_PORT = 80
+
+#class ServerHandler(tornado.web.RequestHandler):
+#	def get(self):
+#		f = open('webapp.html', 'r')
+#		for line in f:
+#			self.write(line)
+
+def mainLoop():
+	print "CHECKPOINT 1", "Listening on port", SERVER_PORT
+	application = tornado.web.Application([ (r"/", tornado.web.StaticFileHandler), ])
+	application.listen(SERVER_PORT)
+	tornado.ioloop.IOLoop.instance().start()
+
+mainLoop()
